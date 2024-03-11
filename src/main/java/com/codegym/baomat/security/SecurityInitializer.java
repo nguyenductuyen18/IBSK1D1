879d2ed0247026b577fd409b42487a0c1c831274
nguyenductuyen18 <nguyenductuyen2k4@gmail.com>
2024-03-11 11:12:01 +0700

    TH_Bao_mat

---
+++ src/main/java/com/codegym/baomat/security/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.codegym.baomat.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
